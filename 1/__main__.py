56b59db86fb6c3061cc64046e7ce01d554c306ae
Visa Pollari <visa.pollari@gmail.com>
2020-12-01 09:57:26 +0200

    Fix pylint

--- 1/__main__.py
+++ 1/__main__.py
@@ -1,36 +1,34 @@
 """
 Advent of Code Challenge 1
 """
-
 import os
-dirname = os.path.dirname(__file__)
 
+dirname = os.path.dirname(__file__)
 with open(os.path.join(dirname, 'input'), 'r+') as file:
     lines = [int(x) for x in file.readlines()]
     lines.sort()
 
-DONE = False
-for ai, a in enumerate(lines):
-    for b in lines[:ai]:
-        if a + b == 2020:
-            print('Part 1')
-            print(f'{a} * {b} = {a * b}')
-            DONE = True
-            break
-    if DONE:
-        break
+def part1():
+    """Solve Part 1"""
+    for aIndex, a in enumerate(lines):
+        for b in lines[:aIndex]:
+            if a + b == 2020:
+                print('Part 1')
+                print(f'{a} * {b} = {a * b}')
+                return a * b
+    return - 1
 
-DONE = False
-for ai, a in enumerate(lines):
-    for bi, b in enumerate(lines[:ai]):
-        for c in lines[:bi]:
-            if a + b + c == 2020:
-                print('Part 2')
-                print(f'{a} * {b} * {c} = {a * b * c}')
-                DONE = True
-                break
-        if DONE:
-            break
-    if DONE:
-        break
+def part2():
+    """Solve Part 2"""
+    for aIndex, a in enumerate(lines):
+        for bIndex, b in enumerate(lines[:aIndex]):
+            for c in lines[:bIndex]:
+                if a + b + c == 2020:
+                    print('Part 2')
+                    print(f'{a} * {b} * {c} = {a * b * c}')
+                    return a * b * c
+    return -1
 
+if __name__ == '__main__':
+    part1()
+    part2()
